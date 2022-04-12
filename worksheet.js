fbc4b8355588b26b1564879d045a8698dfc371e5
Michael J <johnson_michael1329@yahoo.com>
2022-04-12 14:30:15 -0500

    Added for loop examples + if statements.

--- worksheet.js
+++ worksheet.js
@@ -1,12 +1,37 @@
 // DAY OF THE WEEK EXAMPLE
 let dayOfTheWeek = 'Monday';
-console.log(dayOfTheWeek)
+// console.log(dayOfTheWeek)
 dayOfTheWeek = 'friday';
-console.log(`I cant wait until ${dayOfTheWeek}!`)
+// console.log(`I cant wait until ${dayOfTheWeek}!`)
 
 
 // USER INPUT EXAMPLE
+// let animalInput = prompt("Whats your favorite animal? ");
+// let colorInput = prompt("Whats your favorite color? ")
+// console.log(`I've never seen a ${colorInput} ${animalInput}!`)
 
-let animalInput = prompt("Whats your favorite animal? ");
-let colorInput = prompt("Whats your favorite color? ")
-console.log(`I've never seen a ${colorInput} ${animalInput}!`)
+// CONDITIONALS
+// let timeOfDay = 1200
+// let mealTime = '';
+// if (timeOfDay === 1200) {
+//     console.log(`Its time for ABE`)
+// } else if (timeOfDay > 1200 && timeOfDay < 1700) {
+//     console.log(`Its time for Chick Fil A`)
+// } else if (timeOfDay > 1700) {
+//     console.log(`Its time for Sotos`)
+// } else {
+//     console.log(`You cant eat during this time! `)
+// }
+
+// FOR LOOPS
+for(var i = 0; i < 7; i++){
+  console.log('JavaScript is Cool !')
+}
+for(var i = -1; i < 10; i++){
+    console.log(i+1)
+}
+
+for(var i = 0; i < 5; i++){
+    console.log('hello')
+    console.log('goodbye')
+}
