a3cc80eb5254f7ac1946047a5c4db96902cbb653
Michael J <johnson_michael1329@yahoo.com>
2022-04-12 11:31:34 -0500

    Added a prompt method that prints to the console.

--- worksheet.js
+++ worksheet.js
@@ -1,6 +1,12 @@
-
+// DAY OF THE WEEK EXAMPLE
 let dayOfTheWeek = 'Monday';
 console.log(dayOfTheWeek)
 dayOfTheWeek = 'friday';
 console.log(`I cant wait until ${dayOfTheWeek}!`)
 
+
+// USER INPUT EXAMPLE
+
+let animalInput = prompt("Whats your favorite animal? ");
+let colorInput = prompt("Whats your favorite color? ")
+console.log(`I've never seen a ${colorInput} ${animalInput}!`)
